8a8847243ad3c0263988ac3dab77c77cfbf5d6c9
Mündtrath <mikemuendtrath@web.de>
2024-03-20 19:06:12 +0100

    save

--- src/main/java/com/example/project/security/SecurityConfig.java
+++ src/main/java/com/example/project/security/SecurityConfig.java
@@ -35,10 +35,9 @@ public class SecurityConfig{
 						            .requestMatchers("/aufgaben/save").hasAnyRole("ADMIN", "USER")
 						            .requestMatchers("/aufgaben/delete").hasRole("ADMIN")
 						            .requestMatchers("/aufgaben/update").hasAnyRole("ADMIN", "USER")
-                                    // .requestMatchers("/images/**", "/css/**", "/js/**", "/WEB-INF/views/**","/**","/","/*").permitAll()  
+                                    .requestMatchers("/images/**", "/css/**", "/js/**", "/WEB-INF/views/**","/**","/","/*").permitAll()  
 			                        .anyRequest().authenticated())
             .formLogin(form -> form
-        
 		                .loginPage("/index").permitAll()
 		                .loginProcessingUrl("/authenticateTheUser")
 		                )
