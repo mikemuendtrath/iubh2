be5e310e5e4ee798126ceab2cd766d017c69b0c2
Mündtrath <mikemuendtrath@web.de>
2024-03-20 19:53:56 +0100

    save

--- src/main/java/com/example/project/controller/LoginController.java
+++ src/main/java/com/example/project/controller/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("")
+    @GetMapping("/index")
     public String login(){
         return "/index";
     }
